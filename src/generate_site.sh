4df1bbaeca916de6bcb50807689c7d7e7733b1ca
Loren Lisk <loren.lisk@liskl.com>
2020-10-22 13:12:27 -0400

    added crap gallary

--- src/generate_site.sh
+++ src/generate_site.sh
@@ -7,3 +7,5 @@ cat header.tmpl powerwall.tmpl footer.tmpl > ../site/powerwall.html
 cat header.tmpl index.tmpl footer.tmpl > ../site/index.html
 
 cat header.tmpl email.tmpl footer.tmpl > ../site/email.html
+
+cat header.tmpl gallery-liskl-networks-datacenter.tmpl footer.tmpl > ../site/gallery/liskl-networks-datacenter.html
